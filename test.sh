c1bc9d7efc2924580de4d330413d111dd5b25a1a
MeggalBozale <meggalbozale@gmail.com>
2022-05-15 17:45:29 +0100

    Removed things we shouldnt have on the pub repo

--- test.sh
+++
@@ -1,6 +0,0 @@
-for i in 1 2 4 8 16 32 64 128 256 512 1024
-do
-  python3.8 printASCII.py --silent --optimize=$i -o "optimized.txt" ./images/pixelMountains.png
-  stat "optimized.txt" | grep -i 'size' >> test.txt
-  rm "optimized.txt"
-done
